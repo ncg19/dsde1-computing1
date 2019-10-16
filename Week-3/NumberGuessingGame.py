5d8a5f5185272a26452381400cc5e6b4246d70d6
ncg19 <ncg19@ic.ac.uk>
2019-10-16 19:00:26 +0100

    Number Guessing Game with Comments

--- Week-3/NumberGuessingGame.py
+++ Week-3/NumberGuessingGame.py
@@ -7,14 +7,17 @@ randomnumber = random.randint(1,10)
 guess = input('Guess a number between 1 and 10'   )
 guess = int(guess)
 
+#tests if guess is correct, if not, asks for another guess
 if guess == randomnumber:
     print('Yes, you are correct!')
 else:
     guess = int(input('Nope! Guess a different number between 1 and 10'))
+    #tests if the second guess is correct, if not, asks for another guess
     if guess == randomnumber:
         print('Yes, you are correct!')
     else:
         guess = int(input('Nope !Guess a different number between 1 and 10'))
+        #tests if your third guess is correct, if not, informs you that you have lost the game
         if guess == randomnumber:
             print('Yes, you are correct!')
         else:

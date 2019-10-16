ac6289cbcdc5f374642c225e6696c1beabacbf57
ncg19 <ncg19@ic.ac.uk>
2019-10-16 19:24:26 +0100

    Number Guessing Game with elif

--- Week-3/NumberGuessingGame.py
+++ Week-3/NumberGuessingGame.py
@@ -10,16 +10,12 @@ guess = int(guess)
 #tests if guess is correct, if not, asks for another guess
 if guess == randomnumber:
     print('Yes, you are correct!')
+#tests if the second guess is correct, if not, asks for another guess
+elif int(input('Nope! Guess a different number between 1 and 10')) == randomnumber:
+    print('Yes, you are correct!')
+#tests if the third guess is correct, if not, informs you that you have lost the game
+elif int(input('Nope !Guess a different number between 1 and 10')) == randomnumber:
+    print('Yes, you are correct!')
 else:
-    guess = int(input('Nope! Guess a different number between 1 and 10'))
-    #tests if the second guess is correct, if not, asks for another guess
-    if guess == randomnumber:
-        print('Yes, you are correct!')
-    else:
-        guess = int(input('Nope !Guess a different number between 1 and 10'))
-        #tests if your third guess is correct, if not, informs you that you have lost the game
-        if guess == randomnumber:
-            print('Yes, you are correct!')
-        else:
-            print('Nope, you loose!')
+    print('Nope, you loose!')
             

df7acd46b66667d2ab5ff15d923a36f3dd084c26
ncg19 <ncg19@ic.ac.uk>
2019-10-27 13:37:45 +0000

    Completed structures.py

--- Week-4/structures.py
+++ Week-4/structures.py
@@ -9,23 +9,35 @@ Simple functions performing operations on basic Python data structures.
 # write a function that returns a list containig the first and the last element
 # of "the_list". 
 def first_and_last(the_list):
-    return []
+    last = len(the_list)-1
+    return [the_list[0], the_list[last]]
 
 
 # write a function that returns part of "the_list" between indices given by the
 # second and third parameter, respectively. The returned part should be in
 # reverse order than in the original "the_list". 
-# If "end" is greater then "beginning" or any og the indices is out of the
+# If "end" is greater then "beginning" or any of the indices is out of the
 # list, raise a "ValueError" exception. 
 def part_reverse(the_list, beginning, end):
-    return # hint this is incomplete
+    if end < beginning:
+        raise ValueError
+    elif beginning < 0:
+        raise ValueError
+    elif end > len(the_list):
+        raise ValueError
+    else:
+        newlist = the_list[beginning:end]
+        newlist.reverse()
+        return newlist
 
 
 # write a function that at the "index" of "the_list" inserts three times the
 # same value. For example if the_list = [0,1,2,3,4] and index = 3 the function
 # will return [0,1,2,3,3,3,4]. 
 def repeat_at_index(the_list, index):
-    return
+    the_list.insert(index, the_list[index])
+    the_list.insert(index, the_list[index])
+    return the_list
 
 
 # Strings
@@ -33,14 +45,21 @@ def repeat_at_index(the_list, index):
 # write a function that checks whether the word is a palindrome, i.e. it reads
 # the same forward and backwards
 def palindrome_word(word):
-    return
+    word = word.lower()
+    return (word[::-1] == word)
 
 # write a function that checks whether the sentence is a palindrome, i.e. it
 # read the same forward and backward. Ignore all spaces and other characters
 # like fullstops, commas, etc. Also do not consider whether the letter is
 # capital or not. 
 def palindrome_sentence(sentence):
-    return
+    sentence = sentence.lower()
+    sentence = sentence.replace(' ', '')
+    sentence = sentence.replace('.', '')
+    sentence = sentence.replace(',', '')
+    sentence = sentence.replace('?', '')
+    sentence = sentence.replace('!', '')
+    return (sentence[::-1] == sentence)
 
 # write a function that concatenates two sentences. First the function checks
 # whether the sentence meets the following criteria: it starts with a capital
@@ -49,8 +68,14 @@ def palindrome_sentence(sentence):
 # the end.  The concatenated sentence must have no white space at the beginning
 # or at the end and the must be exactly one space after the end of the first
 # sentence. 
-def concatenate_sentences(sentenece1, sentence2):
-    return
+def concatenate_sentences(sentence1, sentence2):
+    sentence1 = sentence1.strip()
+    sentence1 = sentence1 + ' '
+    sentence2 = sentence2.strip()
+    if sentence1[0].isupper() and sentence1[len(sentence1)-2] == '.' and sentence2[0].isupper() and sentence2[len(sentence2)-1] == '.':
+        return sentence1 + sentence2
+    else:
+        raise ValueError
 
 
 # Dictionaries
@@ -58,14 +83,22 @@ def concatenate_sentences(sentenece1, sentence2):
 # write a function that checks whether there is a record with given key in the
 # dictionary. Return True or False.
 def index_exists(dictionary, key):
-    return
+    if key in dictionary:
+        return True
+    else:
+        return False
 
 # write a function which checks whether given value is stored in the
 # dictionary. Return True or False.
 def value_exists(dictionary, value):
-    return
+    allvalues = dictionary.values()
+    if value in allvalues:
+        return True
+    else:
+        return False
 
 # write a function that returns a new dictionary which contains all the values
 # from dictionary1 and dictionary2.
 def merge_dictionaries(dictionary1, dictionary2):
-    return
+    dictionary1.update(dictionary2)
+    return dictionary1
